90a545d56d1e55079d426c4054489b379a1b2443
Andre Oliveira <dezin-oliveira@hotmail.com>
2024-04-02 16:36:14 -0300

    e

--- Forms/frmCrudClientes.Designer.cs
+++ Forms/frmCrudClientes.Designer.cs
@@ -49,11 +49,13 @@
             btnSair = new Button();
             lblTitle = new Label();
             pnlFormLoader = new Panel();
+            groupBox1 = new GroupBox();
             txtDataFechamento = new Class.CustomDatePicker();
             txtDataEntrada = new Class.CustomDatePicker();
             btnCancelar = new FontAwesome.Sharp.IconButton();
             btnConfirmar = new FontAwesome.Sharp.IconButton();
             pnlFormLoader.SuspendLayout();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // lblObservacao
@@ -61,7 +63,7 @@
             lblObservacao.AutoSize = true;
             lblObservacao.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblObservacao.ForeColor = Color.White;
-            lblObservacao.Location = new Point(264, 373);
+            lblObservacao.Location = new Point(39, 328);
             lblObservacao.Name = "lblObservacao";
             lblObservacao.Size = new Size(124, 25);
             lblObservacao.TabIndex = 58;
@@ -72,7 +74,7 @@
             lblDataFechamento.AutoSize = true;
             lblDataFechamento.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblDataFechamento.ForeColor = Color.White;
-            lblDataFechamento.Location = new Point(568, 312);
+            lblDataFechamento.Location = new Point(343, 269);
             lblDataFechamento.Name = "lblDataFechamento";
             lblDataFechamento.Size = new Size(163, 25);
             lblDataFechamento.TabIndex = 57;
@@ -83,7 +85,7 @@
             lblHoras.AutoSize = true;
             lblHoras.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblHoras.ForeColor = Color.White;
-            lblHoras.Location = new Point(261, 312);
+            lblHoras.Location = new Point(43, 268);
             lblHoras.Name = "lblHoras";
             lblHoras.Size = new Size(153, 25);
             lblHoras.TabIndex = 56;
@@ -94,7 +96,7 @@
             lblStatus.AutoSize = true;
             lblStatus.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblStatus.ForeColor = Color.White;
-            lblStatus.Location = new Point(494, 183);
+            lblStatus.Location = new Point(270, 129);
             lblStatus.Name = "lblStatus";
             lblStatus.Size = new Size(67, 25);
             lblStatus.TabIndex = 55;
@@ -105,7 +107,7 @@
             lblBackup.AutoSize = true;
             lblBackup.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblBackup.ForeColor = Color.White;
-            lblBackup.Location = new Point(594, 251);
+            lblBackup.Location = new Point(374, 202);
             lblBackup.Name = "lblBackup";
             lblBackup.Size = new Size(78, 25);
             lblBackup.TabIndex = 54;
@@ -116,7 +118,7 @@
             lblIsm.AutoSize = true;
             lblIsm.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblIsm.ForeColor = Color.White;
-            lblIsm.Location = new Point(701, 180);
+            lblIsm.Location = new Point(477, 126);
             lblIsm.Name = "lblIsm";
             lblIsm.Size = new Size(47, 25);
             lblIsm.TabIndex = 53;
@@ -127,7 +129,7 @@
             lblNecessidade.AutoSize = true;
             lblNecessidade.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblNecessidade.ForeColor = Color.White;
-            lblNecessidade.Location = new Point(261, 253);
+            lblNecessidade.Location = new Point(41, 204);
             lblNecessidade.Name = "lblNecessidade";
             lblNecessidade.Size = new Size(121, 25);
             lblNecessidade.TabIndex = 52;
@@ -138,7 +140,7 @@
             lblDataEntrada.AutoSize = true;
             lblDataEntrada.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblDataEntrada.ForeColor = Color.White;
-            lblDataEntrada.Location = new Point(261, 183);
+            lblDataEntrada.Location = new Point(37, 129);
             lblDataEntrada.Name = "lblDataEntrada";
             lblDataEntrada.Size = new Size(81, 25);
             lblDataEntrada.TabIndex = 51;
@@ -149,7 +151,7 @@
             lblNome.AutoSize = true;
             lblNome.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblNome.ForeColor = Color.White;
-            lblNome.Location = new Point(375, 125);
+            lblNome.Location = new Point(153, 54);
             lblNome.Name = "lblNome";
             lblNome.Size = new Size(66, 25);
             lblNome.TabIndex = 50;
@@ -160,7 +162,7 @@
             lblId.AutoSize = true;
             lblId.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblId.ForeColor = Color.White;
-            lblId.Location = new Point(261, 124);
+            lblId.Location = new Point(39, 53);
             lblId.Name = "lblId";
             lblId.Size = new Size(41, 25);
             lblId.TabIndex = 49;
@@ -175,7 +177,7 @@
             cbxStatus.ForeColor = Color.White;
             cbxStatus.FormattingEnabled = true;
             cbxStatus.Items.AddRange(new object[] { "Finalizada", "Em andamento", "Parada", "Cancelada" });
-            cbxStatus.Location = new Point(567, 183);
+            cbxStatus.Location = new Point(343, 129);
             cbxStatus.Name = "cbxStatus";
             cbxStatus.Size = new Size(127, 29);
             cbxStatus.TabIndex = 3;
@@ -189,7 +191,7 @@
             cbxBackup.ForeColor = Color.White;
             cbxBackup.FormattingEnabled = true;
             cbxBackup.Items.AddRange(new object[] { "Cloud", "Pen drive", "HD externo", "Sem backup" });
-            cbxBackup.Location = new Point(678, 251);
+            cbxBackup.Location = new Point(458, 202);
             cbxBackup.Name = "cbxBackup";
             cbxBackup.Size = new Size(186, 29);
             cbxBackup.TabIndex = 6;
@@ -203,7 +205,7 @@
             cbxIsm.ForeColor = Color.White;
             cbxIsm.FormattingEnabled = true;
             cbxIsm.Items.AddRange(new object[] { "André", "Edivania" });
-            cbxIsm.Location = new Point(751, 182);
+            cbxIsm.Location = new Point(527, 128);
             cbxIsm.Name = "cbxIsm";
             cbxIsm.Size = new Size(113, 29);
             cbxIsm.TabIndex = 4;
@@ -219,7 +221,7 @@
             cbxNecessidade.FormattingEnabled = true;
             cbxNecessidade.ItemHeight = 21;
             cbxNecessidade.Items.AddRange(new object[] { "Fiscal", "Gerencial", "Fiscal/Gerencial" });
-            cbxNecessidade.Location = new Point(392, 253);
+            cbxNecessidade.Location = new Point(172, 204);
             cbxNecessidade.Name = "cbxNecessidade";
             cbxNecessidade.Size = new Size(179, 29);
             cbxNecessidade.TabIndex = 5;
@@ -229,7 +231,7 @@
             txtObservacao.BackColor = Color.FromArgb(46, 51, 73);
             txtObservacao.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             txtObservacao.ForeColor = Color.White;
-            txtObservacao.Location = new Point(264, 401);
+            txtObservacao.Location = new Point(39, 356);
             txtObservacao.MaxLength = 255;
             txtObservacao.Multiline = true;
             txtObservacao.Name = "txtObservacao";
@@ -241,7 +243,7 @@
             txtHoras.BackColor = Color.FromArgb(46, 51, 73);
             txtHoras.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             txtHoras.ForeColor = Color.White;
-            txtHoras.Location = new Point(420, 312);
+            txtHoras.Location = new Point(202, 268);
             txtHoras.Name = "txtHoras";
             txtHoras.PlaceholderText = "00:00:00";
             txtHoras.Size = new Size(124, 33);
@@ -253,7 +255,7 @@
             txtNome.CharacterCasing = CharacterCasing.Upper;
             txtNome.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             txtNome.ForeColor = Color.White;
-            txtNome.Location = new Point(447, 124);
+            txtNome.Location = new Point(222, 51);
             txtNome.MaxLength = 80;
             txtNome.Name = "txtNome";
             txtNome.Size = new Size(417, 33);
@@ -264,7 +266,7 @@
             txtId.BackColor = Color.FromArgb(46, 51, 73);
             txtId.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             txtId.ForeColor = Color.White;
-            txtId.Location = new Point(304, 122);
+            txtId.Location = new Point(82, 51);
             txtId.MaxLength = 4;
             txtId.Name = "txtId";
             txtId.Size = new Size(65, 33);
@@ -297,36 +299,48 @@
             // 
             // pnlFormLoader
             // 
+            pnlFormLoader.Controls.Add(groupBox1);
             pnlFormLoader.Controls.Add(btnSair);
-            pnlFormLoader.Controls.Add(txtDataFechamento);
-            pnlFormLoader.Controls.Add(txtDataEntrada);
-            pnlFormLoader.Controls.Add(btnCancelar);
-            pnlFormLoader.Controls.Add(btnConfirmar);
-            pnlFormLoader.Controls.Add(lblBackup);
-            pnlFormLoader.Controls.Add(lblObservacao);
-            pnlFormLoader.Controls.Add(lblIsm);
-            pnlFormLoader.Controls.Add(lblId);
-            pnlFormLoader.Controls.Add(lblNecessidade);
-            pnlFormLoader.Controls.Add(lblDataFechamento);
-            pnlFormLoader.Controls.Add(lblDataEntrada);
             pnlFormLoader.Controls.Add(lblTitle);
-            pnlFormLoader.Controls.Add(lblHoras);
-            pnlFormLoader.Controls.Add(cbxBackup);
-            pnlFormLoader.Controls.Add(txtId);
-            pnlFormLoader.Controls.Add(cbxIsm);
-            pnlFormLoader.Controls.Add(lblStatus);
-            pnlFormLoader.Controls.Add(cbxNecessidade);
-            pnlFormLoader.Controls.Add(lblNome);
-            pnlFormLoader.Controls.Add(txtNome);
-            pnlFormLoader.Controls.Add(txtObservacao);
-            pnlFormLoader.Controls.Add(txtHoras);
-            pnlFormLoader.Controls.Add(cbxStatus);
             pnlFormLoader.Dock = DockStyle.Fill;
             pnlFormLoader.Location = new Point(0, 0);
             pnlFormLoader.Name = "pnlFormLoader";
             pnlFormLoader.Size = new Size(1114, 700);
             pnlFormLoader.TabIndex = 59;
             pnlFormLoader.Paint += pnlFormLoader_Paint;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(cbxNecessidade);
+            groupBox1.Controls.Add(cbxStatus);
+            groupBox1.Controls.Add(txtDataFechamento);
+            groupBox1.Controls.Add(txtHoras);
+            groupBox1.Controls.Add(txtDataEntrada);
+            groupBox1.Controls.Add(txtObservacao);
+            groupBox1.Controls.Add(btnCancelar);
+            groupBox1.Controls.Add(txtNome);
+            groupBox1.Controls.Add(btnConfirmar);
+            groupBox1.Controls.Add(lblNome);
+            groupBox1.Controls.Add(lblBackup);
+            groupBox1.Controls.Add(lblStatus);
+            groupBox1.Controls.Add(lblObservacao);
+            groupBox1.Controls.Add(cbxIsm);
+            groupBox1.Controls.Add(lblIsm);
+            groupBox1.Controls.Add(txtId);
+            groupBox1.Controls.Add(lblId);
+            groupBox1.Controls.Add(cbxBackup);
+            groupBox1.Controls.Add(lblNecessidade);
+            groupBox1.Controls.Add(lblHoras);
+            groupBox1.Controls.Add(lblDataFechamento);
+            groupBox1.Controls.Add(lblDataEntrada);
+            groupBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.ForeColor = Color.White;
+            groupBox1.Location = new Point(222, 74);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(682, 566);
+            groupBox1.TabIndex = 59;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Dados cadastrais";
             // 
             // txtDataFechamento
             // 
@@ -335,7 +349,7 @@
             txtDataFechamento.CustomFormat = "yyyy-MM-dd";
             txtDataFechamento.Font = new Font("Segoe UI", 11F);
             txtDataFechamento.Format = DateTimePickerFormat.Custom;
-            txtDataFechamento.Location = new Point(731, 309);
+            txtDataFechamento.Location = new Point(506, 266);
             txtDataFechamento.MinimumSize = new Size(0, 35);
             txtDataFechamento.Name = "txtDataFechamento";
             txtDataFechamento.Size = new Size(133, 35);
@@ -352,7 +366,7 @@
             txtDataEntrada.CustomFormat = "yyyy-MM-dd";
             txtDataEntrada.Font = new Font("Segoe UI", 11F);
             txtDataEntrada.Format = DateTimePickerFormat.Custom;
-            txtDataEntrada.Location = new Point(339, 180);
+            txtDataEntrada.Location = new Point(115, 126);
             txtDataEntrada.MinimumSize = new Size(0, 35);
             txtDataEntrada.Name = "txtDataEntrada";
             txtDataEntrada.Size = new Size(127, 35);
@@ -371,7 +385,7 @@
             btnCancelar.IconFont = FontAwesome.Sharp.IconFont.Auto;
             btnCancelar.IconSize = 28;
             btnCancelar.ImageAlign = ContentAlignment.MiddleRight;
-            btnCancelar.Location = new Point(568, 544);
+            btnCancelar.Location = new Point(343, 499);
             btnCancelar.Name = "btnCancelar";
             btnCancelar.Size = new Size(104, 38);
             btnCancelar.TabIndex = 11;
@@ -391,7 +405,7 @@
             btnConfirmar.IconFont = FontAwesome.Sharp.IconFont.Auto;
             btnConfirmar.IconSize = 30;
             btnConfirmar.ImageAlign = ContentAlignment.MiddleRight;
-            btnConfirmar.Location = new Point(447, 544);
+            btnConfirmar.Location = new Point(222, 499);
             btnConfirmar.Name = "btnConfirmar";
             btnConfirmar.Size = new Size(104, 38);
             btnConfirmar.TabIndex = 10;
@@ -413,6 +427,8 @@
             Load += frmCrudClientes_Load;
             pnlFormLoader.ResumeLayout(false);
             pnlFormLoader.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -443,5 +459,6 @@
         public FontAwesome.Sharp.IconButton btnCancelar;
         public Class.CustomDatePicker txtDataFechamento;
         public Class.CustomDatePicker txtDataEntrada;
+        private GroupBox groupBox1;
     }
 }
